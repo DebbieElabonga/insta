d85dc50e31cfeeedc9f1f94d12a66a672785a81f
DebbieElabonga <debbie.elabonga@student.moringaschool.com>
2021-07-11 08:20:22 +0300

    logout and login

--- insta/settings.py
+++ insta/settings.py
@@ -131,6 +131,11 @@ STATICFILES_DIRS = [
 ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_REDIRECT_URL = 'index'
+
+LOGOUT_REDIRECT_URL = 'signup'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
